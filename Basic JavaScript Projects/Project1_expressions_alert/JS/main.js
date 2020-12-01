782f8744ca1824f3b7ae8105d2cb6b1a5e7e1bee
Brooks Radtke <brooks@brooksradtke.com>
2020-12-01 10:53:25 -0800

    Push JavaScript Projects Folder

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,20 @@
+//window.alert("Hello, World!");
+
+//document.write("Hello, World!");
+
+//var a = "This is a string";
+//document.write(a);
+
+//var b = "Hi there!";
+//window.alert(b);
+
+//concatinating example
+//var c = "And she said, \"You must!\" And I said, \"I won't.\"" + " In the end, I did." + " And we were happy all the same.";
+//document.write(c);
+
+//var dwarfs = "The Boyz", d1 = "Doc", d2 = "Dopey", d3 = "Happy", 
+//d4 = "Grumpy", d5 = "Bashful", d6 = "Sneezy", d7 = "Sleepy";
+//var dwarfs = dwarfs.fontcolor("red");
+//document.write(dwarfs);
+
+//document.write(3+3);
